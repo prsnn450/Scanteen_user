a8f7b2af5d95672449442f847db05833221a383d
prsnn450 <121123161+prsnn450@users.noreply.github.com>
2023-04-28 20:58:09 +0530

    Add files via upload

--- routers/user.js
+++ routers/user.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const Signup= require('../models/signup.js');
 const jwt = require('jsonwebtoken');
 const organisation= require('../models/organisation.js');
+const mongoose=require('mongoose');
 //const JWT_SECRET = 'secret';
 
 router.post('/User/signUp',async function(req,res){
@@ -11,7 +12,7 @@ router.post('/User/signUp',async function(req,res){
         Name: req.body.Name,
         phoneNumber: req.body.phoneNumber,
         password: req.body.password,
-        orgId: req.body.orgId
+        orgId: new mongoose.Types.ObjectId(req.body.orgId)
     });
     const existlogin= await Signup.findOne({phoneNumber: newlogin.phoneNumber});
     if(existlogin){
@@ -19,7 +20,7 @@ router.post('/User/signUp',async function(req,res){
     }
     else{
         await newlogin.save();
-        const org= await organisation.findOne({orgId: newlogin.orgId});
+        const org= await organisation.findOne({_id: newlogin.orgId});
         //res.json({Name:newlogin.name ,phoneNumber:newlogin.phoneNumber,orgId:newlogin.orgId,orgIp:org.orgIp});
         //const token= jwt.sign({userId: newlogin._id}, JWT_SECRET);
         //res.json({token});
@@ -45,7 +46,7 @@ router.post('/user/login',async function(req,res){
     const exist= await Signup.findOne({phoneNumber:login.phoneNumber});
     if(exist!=null ){
         if(login.password==exist.password){
-            const org= await organisation.findOne({orgId: exist.orgId});
+            const org= await organisation.findOne({_id: exist.orgId});
             //res.json({Name:exist.Name,phoneNumber:exist.phoneNumber,orgId:exist.orgId,orgIp:org.orgIp});
             //const token= jwt.sign({userId: exist._id}, JWT_SECRET);
            // res.json({token});
@@ -89,9 +90,9 @@ router.post('/user/update',authenticateToken,async function(req,res){
         Name: req.body.Name,
         phoneNumber: req.body.phoneNumber,
         password: req.body.password,
-        orgId: req.body.orgId
+        orgId: new mongoose.Types.ObjectId(req.body.orgId)
     });
-    const org= await organisation.findOne({orgId: newupdate.orgId});
+    const org= await organisation.findOne({_id: newupdate.orgId});
     await Signup.findByIdAndUpdate(req.userId, {Name:newupdate.Name,phoneNumber: newupdate.phoneNumber,password:newupdate.password,orgId:newupdate.orgId});
     //res.json({token});
     //res.json({name:newupdate.Name ,phoneNumber:newupdate.phoneNumber,orgId:newupdate.orgId,orgIp:org.orgIp});
